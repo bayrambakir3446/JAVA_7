ceda4a4da58f06fe3c1fc7a6c740df81d468e72a
offaruk <faruk.topak@hotmail.com>
2022-10-21 15:38:01 +0100

    first day

---
+++ src/gunler/gun1/Hello.java
@@ -0,0 +1,14 @@
+package gunler.gun1;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Batch 7 ");
+        System.out.println("Hello Again");
+
+
+    }
+
+
+}

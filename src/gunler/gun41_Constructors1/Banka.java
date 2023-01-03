ba5e3fb557fd5650fbe0060e9e0674739b58fa7c
Armada <yaskan@gmail.com>
2023-01-04 00:05:17 +0300

    Armada

--- src/gunler/gun41_Constructors1/Banka.java
+++ src/gunler/gun41_Constructors1/Banka.java
@@ -12,4 +12,41 @@ public class Banka {
         this.adi = adi;
         this.adresi = adresi;
     }
+
+    public void musteriEkle(Musteri musteri){
+        this.hesapNo++;
+        musteri.hesapNo = this.hesapNo;
+        this.musteriler.add(musteri);
+        System.out.println(musteri.hesapNo + " hesap nolu " + musteri.adi + " isimli müşteri bankaya eklendi.");
+    }
+
+    public void paraCek(Musteri musteri,double paraMiktari){
+        if(musteri.bakiye >= paraMiktari){
+           // musteri.bakiye = musteri.bakiye - paraMiktari;
+            musteri.bakiye -= paraMiktari;
+            musteri.showInfo();
+        }else {
+            System.out.println("Bakiyeniz " + paraMiktari + " TL para çekmeye müsait değil!");
+            System.out.println("Enfazla " + musteri.bakiye + " TL kadar para çekebilirsin.");
+        }
+    }
+
+    public void showInfo(){
+        System.out.println("Bankamızdki müşterilerin bilgileri aşağıdadır.");
+        System.out.println("----------------------------------------------");
+        System.out.println("Bankanın adı   : " + this.adi);
+        System.out.println("Bankanın adresi: " + this.adresi);
+        System.out.println("----------------------------------------------");
+        for (Musteri musteri:this.musteriler){
+           /* System.out.println("**** Müşteri Bilgileri ****");
+            System.out.println("----------------------------");
+            System.out.println("Müşterinin adı           : " + musteri.adi);
+            System.out.println("Müşterinin kimlik no     : " + musteri.kimlikNo) ;
+            System.out.println("Müşterinin hesp no       : "  + musteri.hesapNo);
+            System.out.println("Müşterinin telefon no    : " + musteri.telefonNo);
+            System.out.println("Müşterinin hesap bakiyesi: " + musteri.bakiye);*/
+
+            musteri.showInfo();
+        }
+    }
 }
--- src/gunler/gun41_Constructors1/BankaProgram.java
+++ src/gunler/gun41_Constructors1/BankaProgram.java
@@ -9,6 +9,18 @@ public class BankaProgram {
         musteri2.showInfo();
 
         Musteri musteri3 = new Musteri("Mehmet John",12345,34567,903456789,5000);
-       musteri3.showInfo();
+        musteri3.showInfo();
+
+       Banka banka = new Banka("ABC International Bank","Berlin/Germany");
+       banka.musteriEkle(musteri3);
+       banka.musteriEkle(musteri2);
+       banka.musteriEkle(musteri1);
+
+       banka.paraCek(musteri3,1500);
+        banka.paraCek(musteri3,1500);
+        banka.paraCek(musteri3,2500);
+
+     //  banka.showInfo();
+
     }
 }

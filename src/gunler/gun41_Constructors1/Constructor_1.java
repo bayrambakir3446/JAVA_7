bf3ae393538de72fc8dd8844c8ccb23cf087f577
Armada <yaskan@gmail.com>
2023-01-03 19:52:57 +0300

    Armada

---
+++ src/gunler/gun41_Constructors1/Constructor_1.java
@@ -0,0 +1,7 @@
+package gunler.gun41_Constructors1;
+
+public class Constructor_1 {
+    public static void main(String[] args) {
+        System.out.println("Constructors and passing objects to methods in Java.");
+    }
+}

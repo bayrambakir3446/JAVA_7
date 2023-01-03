9433445ef2e8c7d04912cc708afffbfdf007b6e4
Armada <yaskan@gmail.com>
2023-01-03 22:19:58 +0300

    Armada

---
+++ src/gunler/gun41_Constructors1/Circle.java
@@ -0,0 +1,30 @@
+package gunler.gun41_Constructors1;
+
+public class Circle {
+   double radius;
+
+   //No-argument Constructor
+   Circle(){
+       System.out.println("Default no-argument constructor called/invoked");
+       radius = 1;
+   }
+
+   //Paremeter constructor
+   Circle(double yariCap){
+       System.out.println("Parameterize constructor called/invoked ");
+       radius = yariCap;
+   }
+
+  /* double getArea(){
+      double area = radius * radius * Math.PI;
+   return area;
+   }*/
+   double getArea(){
+      return radius * radius * Math.PI;
+   }
+
+   double getPerimeter(){
+      return 2 * radius * Math.PI;
+   }
+
+}
---
+++ src/gunler/gun41_Constructors1/CircleTest.java
@@ -0,0 +1,20 @@
+package gunler.gun41_Constructors1;
+
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle daire1 = new Circle();
+       // daire1.radius = 3;
+        System.out.println(daire1.getArea());
+        double alan = daire1.getArea();
+        System.out.println(daire1.radius + " yarıçaplı Dairenin alanı = " + alan);
+        System.out.println(daire1.radius + " yarıçaplı Dairenin çevresi = " + daire1.getPerimeter());
+
+        Circle daire2 = new Circle(3.5);
+
+        System.out.println("daire2.radius = " + daire2.radius);
+        System.out.println(daire2.radius + " yarıçaplı Dairenin alanı = " + daire2.getArea());
+        System.out.println(daire2.radius + " yarıçaplı Dairenin çevresi = " + daire2.getPerimeter());
+
+
+    }
+}

cfc38b0b5c6ff2df117b02260e16e187212f2d1e
Armada <yaskan@gmail.com>
2023-01-04 23:53:04 +0300

    Armada

---
+++ src/gunler/gun42_Constructors2/Student/Course.java
@@ -0,0 +1,8 @@
+package gunler.gun42_Constructors2.Student;
+
+public class Course {
+    String name;
+    int maxCapacity;
+    int emptyCapacity;
+
+}
---
+++ src/gunler/gun42_Constructors2/Student/CourseProgram.java
@@ -0,0 +1,7 @@
+package gunler.gun42_Constructors2.Student;
+
+public class CourseProgram {
+    public static void main(String[] args) {
+
+    }
+}

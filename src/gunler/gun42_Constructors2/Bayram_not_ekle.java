88b8d439590ae154fd9032d3af152d3ba4b08884
BayramBakir <you@example.com>
2023-01-07 23:53:43 +0100

    bayram eklemeli

--- src/gunler/gun42_Constructors2/Bayram_not_ekle.java
+++ src/gunler/gun42_Constructors2/Bayram_not_ekle.java
@@ -3,5 +3,7 @@ package gunler.gun42_Constructors2;
 public class Bayram_not_ekle {
     public static void main(String[] args) {
         System.out.println("hadi bakalim");
+        System.out.println("bakalim gidiyormu");
     }
+
 }
